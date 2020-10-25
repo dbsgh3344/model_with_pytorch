7cad812219e94bff769a670f14c23c8529a34801
dbsgh3344 <dbsgh3344@naver.com>
2020-10-25 18:18:08 +0900

    add setup

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'DataFrame_Generate',
+    version          = '1.0',
+    description      = 'DataFrame Generate',
+    author           = 'Yun Ho Song',
+    author_email     = 'dbsgh3322@gmail.com',
+    url              = 'https://github.com/dbsgh3344/model_with_pytorch',
+    install_requires = [ ]),
+    python_requires  = '>=3',
